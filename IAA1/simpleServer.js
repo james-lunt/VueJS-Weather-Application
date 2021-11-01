eb3378bfb622be613d9d2813e25a68a96ae51d8c
James Lunt <50912137+james-lunt@users.noreply.github.com>
2021-11-01 21:11:48 +0000

    Humidity levels reported based on coordinate location.
    Air Pollution API utilised to report PM25 humidity levels at a certain location for the next 5 days. If the humidity level exceeds 10, tell user to wear a mask.

--- IAA1/simpleServer.js
+++ IAA1/simpleServer.js
@@ -1,4 +1,5 @@
 const API_KEY = "2851461c73782e2d4b65647c123fa135"
+const POLLUTION_API_KEY = "6bf603bc2a5ef6d74967fcdac63cbe4c"
 const express = require('express')
 const axios = require('axios')
 const app = express()
@@ -12,6 +13,8 @@ app.get('/weather/:city', getWeather)
 app.get('/forecast/:city',getForecast)
 app.get('/temperature/:city',getTemperature)
 app.get('/table/:city',getForecast)
+app.get('/coords/:city',getCoordinates)
+app.get('/pollution/:lat/:lon',getAirPollution)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
@@ -29,9 +32,23 @@ async function getForecast(req,res){
 
 async function getTemperature(req,res){
     let response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=${API_KEY}`)
-    //temperature = response.data.main
     temperature = response.data.main.temp
     res.json({temperature})
+}
+
+async function getCoordinates(req,res){
+    let response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${req.params.city}&appid=${API_KEY}`)
+    coords = response.data.coord
+    res.json({coords})
+}
+
+async function getAirPollution(req,res){
+    let response = await axios.get(`http://api.openweathermap.org/data/2.5/air_pollution/forecast?lat=${req.params.lat}&lon=${req.params.lon}&appid=${API_KEY}`)
+    pollution = response.data.list
+    //console.log(pollution)
+    //console.log("hello")
+    //console.log(pollution[0].components.pm2_5)
+    res.json(pollution)
 }
 
 
